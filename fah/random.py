e319822b67b4d9e1aa4565d0d68b3a9e10c7e43f
jac <crowja@gmail.com>
2022-01-17 10:22:32 -0600

    Added.

---
+++ fah/random.py
@@ -0,0 +1,112 @@
+#! /usr/bin/env python3
+
+import argparse
+import math
+import random
+import re
+import sys
+import fah.utils
+
+
+def main():
+
+    # Character sets for sequence types. Noise symbol X/N needs to be final character.
+    charset = {
+        "DNA": [c for c in "ACGTN"],
+        "PEPTIDE": [c for c in "ACDEFGHIKLMNPQRSTVWYX"],
+        "RNA": [c for c in "ACGUN"],
+    }
+
+    ap = argparse.ArgumentParser(
+        description="Generate random sequences in fasta format.",
+        epilog="Specify SEED to generate the same sequences.",
+    )
+    ap.add_argument("-L", "--label", default=False, help="prefix for the sequence ids")
+    ap.add_argument(
+        "-l",
+        "--len",
+        type=int,
+        default=100,
+        help="mean length of output sequences, default 100",
+    )
+    ap.add_argument(
+        "-n",
+        "--num",
+        type=int,
+        default=1,
+        help="number of sequences to generate, default 1",
+    )
+    ap.add_argument(
+        "-N", "--noise", type=float, default=0.0, help="noise percentage, default 0.0"
+    )
+    ap.add_argument(
+        "-r",
+        "--rand",
+        action="store_true",
+        default=False,
+        help="sequences have random length with mean LEN",
+    )
+    ap.add_argument(
+        "-s", "--seed", type=int, default=False, help="random number generator seed"
+    )
+    ap.add_argument(
+        "-t",
+        "--type",
+        choices=["DNA", "RNA", "PEPTIDE"],
+        default="DNA",
+        help="type of sequence(s) to generate, default DNA",
+    )
+    args = ap.parse_args()
+
+    prefix = ""
+
+    if args.label:
+        tmp = re.sub(r"\W+", "_", args.label.strip())
+        if tmp:  # skip if args.label was all whitespace.
+            prefix = tmp + ":"
+
+    if args.noise < 0 or args.noise > 100:
+        print(
+            f"[ERROR] noise percentage needs to be between 0 and 100, specified {args.noise}",
+            file=sys.stderr,
+        )
+        exit(1)
+
+    if args.len < 1:
+        print(
+            f"[ERROR] target sequence length needs to be greater than 0, specified {args.len}",
+            file=sys.stderr,
+        )
+        exit(1)
+
+    w0 = (100.0 - args.noise) / (len(charset[args.type]) - 1)
+    weights = [w0 for _ in range(len(charset[args.type]))]
+    weights[-1] = args.noise
+    ##print(weights); exit(0)
+
+    if args.seed:
+        random.seed(a=args.seed)
+
+    for i in range(args.num):
+        if args.rand:
+            ##seqlen = max(1, np.random.poisson(args.len))
+            z = (random.random() + random.random() + random.random()) / 3  # Bates
+            seqlen = max(1, int(2 * args.len * z))
+        else:
+            seqlen = args.len
+
+        chars = random.choices(charset[args.type], weights=weights, k=seqlen)
+        ##chars = np.random.choice(charset[args.type], size=seqlen, p=probs,)
+        """
+        chars = rng.choice(
+            charset[args.type],
+            size=seqlen,
+            p=probs,
+        )
+        """
+        print(f">{prefix}{i + 1}")
+        print(f"{''.join(chars)}")
+
+
+if __name__ == "__main__":
+    main()
