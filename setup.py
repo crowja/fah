9a55fadfb10fd15aa61a98f604cfac00c292d587
jac <crowja@gmail.com>
2022-02-01 14:03:36 -0600

    Bump version: 1.0.4-dev0 → 1.0.5-dev0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="fah",
-    version="1.0.4-dev0",
+    version="1.0.5-dev0",
     description="Package providing fasta helpers",
     url="https://github.com/crowja/fah",
     author="John A. Crow",
