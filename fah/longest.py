1c040eeeb60a4d9c88c0d0cdd7f83153d074b714
jac <crowja@gmail.com>
2022-04-29 18:24:46 -0500

    Added --reverse option to longest to generate shortest.

--- fah/longest.py
+++ fah/longest.py
@@ -18,15 +18,28 @@ def main():
         help="write the N longest sequences to stdout, default 10",
     )
     ap.add_argument(
+        "-r",
+        "--reverse",
+        action="store_true",
+        default=False,
+        help="reverse the size criteron, that is, print the N shortest sequences",
+    )
+    ap.add_argument(
         "infile", nargs="?", default=False, help="read from INFILE, default stdin"
     )
     args = ap.parse_args()
 
     pq = futils.PriorityQueue(max_capacity=args.n)
 
+    """
+    Sign used for weights in the priority queue, -1 for longest to shortest, +1 for
+    shortest to longest.
+    """
+    s = 1 if args.reverse else -1
+
     try:
         for seq in futils.fasta_reader(args.infile):
-            pq.put(-len(seq[1]), seq)
+            pq.put(s * len(seq[1]), seq)
     except Exception as e:
         print("BARF", e)  # TODO
         pass
