ae82ad6d86c1adad3a08017541ba5740cff78dda
jac <crowja@gmail.com>
2023-06-10 17:11:30 -0500

    .

--- fah/select.py
+++ fah/select.py
@@ -3,15 +3,29 @@ import re
 import sys
 import fah.utils as futils
 
+'''
+ET217190.1	100	200	+	info about ET217190
+seq1	3	5
+'''
+
 def read_file_of_ids(infile):
-    ids = set()
+    ids = {}
     if infile:
         with open(infile, "r") as infh:
             for line in infh:
                 text = line.strip()
-                if not text:
+                if not text or line.startswith("#"):
                     continue
-                ids.add(text)
+                vals = text.split("\t")
+                seqid = vals[0]
+                if seqid not in ids:
+                    ids[seqid] = []
+                if len(vals) < 1:
+                    continue
+                elif len(vals) < 2:
+                    start0 = int(vals[0]) ## TODO
+                    end0 = -1
+                ids[seqid].append(vals[1:-1])
     return ids
 
 def main():
@@ -35,20 +49,23 @@ def main():
     )
     args = ap.parse_args()
 
-    ids = set()
-
     # Get ids specified in an external file.
-    ids.update(read_file_of_ids(args.file))
+    print(f"Reading from {args.file}")
+    ids = read_file_of_ids(args.file)
+    ##print(ids)
+
 
     # Include any ids specified at the command line.
     if args.id:
-        ids.update(set(args.id))
+        ##ids.update(set(args.id))
+        pass
 
     try:
         for seq in futils.fasta_reader(args.infile):
             hh = futils.split_header(seq[0])
             ##print(f"***{hh}***")
             if hh[0] and hh[0] in ids:
+                # Figure out what range to use and which strand ...
                 futils.print_as_fasta(header=seq[0], seqtxt=seq[1])
     except Exception as e:
         print("BARF", e)  # TODO
